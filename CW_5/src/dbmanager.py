aa300b06bf70fb80b0398c2edab256721fb9c99c
Klasis <d23kv321@gmail.com>
2023-09-26 22:45:35 +0300

    commit

--- CW_5/src/dbmanager.py
+++ CW_5/src/dbmanager.py
@@ -1,203 +1,133 @@
-import psycopg2
 import requests
-
-
-def get_all_vacancies():
-    url = "https://api.hh.ru/vacancies"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        data = response.json()
-        vacancies = []
-
-        for vacancy in data["items"]:
-            company_name = vacancy["employer"]["name"]
-            vacancy_title = vacancy["name"]
-            salary = vacancy["salary"]
-            vacancy_link = vacancy["alternate_url"]
-
-            if salary is not None:
-                salary_text = f"{salary['from']} - {salary['to']} {salary['currency']}"
-            else:
-                salary_text = "Не указана"
-
-            vacancies.append({
-                "Компания": company_name,
-                "Вакансия": vacancy_title,
-                "Зарплата": salary_text,
-                "Ссылка": vacancy_link
-            })
-
-        return vacancies
-    else:
-        print("Ошибка при получении данных. Код ошибки:", response.status_code)
-        return None
-
+import psycopg2
 
 class DBManager:
-
-    def __init__(self, db_name: str, db_user: str, db_password: str, db_host: str, db_port: str) -> None:
-        '''
-        Инициализация класса DBManager
-        :param db_name:
-        :param db_user:
-        :param db_password:
-        :param db_host:
-        :param db_port:
-        '''
+    def __init__(self, db_name, db_user, db_password, db_host, db_port):
         self.db_name = db_name
         self.db_user = db_user
         self.db_password = db_password
         self.db_host = db_host
         self.db_port = db_port
-        self.connection = psycopg2.connect(dbname='postgres',
-                                           user=self.db_user,
-                                           password=self.db_password,
-                                           host=self.db_host,
-                                           port=db_port)
-
-    def create_database(self):
-        conn = psycopg2.connect(
-            host="localhost",
-            port="5432",
-            user="postgres",
-            password="080475"
-        )
-        conn.autocommit = True
-        cursor = conn.cursor()
-        cursor.execute("CREATE DATABASE job_data")
-        cursor.close()
-        conn.close()
-
-    def create_tables(self):
-        conn = psycopg2.connect(
-            host="your_host",
-            port="your_port",
-            user="your_user",
-            password="your_password",
-            database="job_data"
-        )
-        conn.autocommit = True
-        cursor = conn.cursor()
-        cursor.execute("""
-            CREATE TABLE employers (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(255),
-                vacancies_count INTEGER
-            )
-        """)
-        cursor.execute("""
-            CREATE TABLE vacancies (
-                id SERIAL PRIMARY KEY,
-                employer_id INTEGER,
-                name VARCHAR(255),
-                salary VARCHAR(255),
-                link VARCHAR(255),
-                FOREIGN KEY (employer_id) REFERENCES employers (id)
-            )
-        """)
-        cursor.close()
-        conn.close()
 
     def get_companies_and_vacancies_count(self):
-        url = "https://api.hh.ru/employers/"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            data = response.json()
-            companies = []
-
-            for employer in data:
-                company_name = employer["name"]
-                vacancies_count = employer["vacancies_count"]
-                companies.append((company_name, vacancies_count))
-
-            return companies
-        else:
-            print("Ошибка при получении данных. Код ошибки:", response.status_code)
-            return None
-
-    def get_avg_salary(self):
         url = "https://api.hh.ru/vacancies"
         response = requests.get(url)
+        data = response.json()
 
-        if response.status_code == 200:
-            data = response.json()
-            salaries = []
-
-            for vacancy in data["items"]:
-                salary = vacancy["salary"]
-
-                if salary is not None:
-                    if salary["currency"] == "RUR":
-                        if salary["from"] is not None and salary["to"] is not None:
-                            avg_salary = (salary["from"] + salary["to"]) / 2
-                        elif salary["from"] is not None:
-                            avg_salary = salary["from"]
-                        elif salary["to"] is not None:
-                            avg_salary = salary["to"]
-                        else:
-                            continue  # Пропускаем вакансии без указания зарплаты
-                        salaries.append(avg_salary)
-
-            if len(salaries) > 0:
-                avg_salary = sum(salaries) / len(salaries)
-                return avg_salary
+        companies = {}
+        for vacancy in data['items']:
+            company = vacancy['employer']['name']
+            if company in companies:
+                companies[company] += 1
             else:
-                return None
+                companies[company] = 1
+
+        return companies
+
+    def get_all_vacancies(self):
+        url = "https://api.hh.ru/vacancies"
+        response = requests.get(url)
+        data = response.json()
+
+        vacancies = []
+        for vacancy in data['items']:
+            company = vacancy['employer']['name']
+            title = vacancy['name']
+            salary = vacancy['salary']
+            link = vacancy['alternate_url']
+            vacancies.append({
+                'company': company,
+                'title': title,
+                'salary': salary,
+                'link': link
+            })
+
+        return vacancies
+
+    def get_avg_salary(self):
+        vacancies = self.get_all_vacancies()
+        total_salary = 0
+        count = 0
+
+        for vacancy in vacancies:
+            salary = vacancy['salary']
+            if salary and salary['currency'] == 'RUR':
+                if salary['from']:
+                    total_salary += salary['from']
+                    count += 1
+                if salary['to']:
+                    total_salary += salary['to']
+                    count += 1
+
+        if count > 0:
+            avg_salary = total_salary / count
+            return avg_salary
         else:
-            print("Ошибка при получении данных. Код ошибки:", response.status_code)
             return None
 
     def get_vacancies_with_higher_salary(self):
-        url = "https://api.hh.ru/vacancies"
-        response = requests.get(url)
+        avg_salary = self.get_avg_salary()
+        vacancies = self.get_all_vacancies()
+        high_salary_vacancies = []
 
-        if response.status_code == 200:
-            data = response.json()
-            salaries = []
-            vacancies = []
+        for vacancy in vacancies:
+            salary = vacancy['salary']
+            if salary and salary['currency'] == 'RUR':
+                if salary['from'] and salary['from'] > avg_salary:
+                    high_salary_vacancies.append(vacancy)
+                if salary['to'] and salary['to'] > avg_salary:
+                    high_salary_vacancies.append(vacancy)
 
-            for vacancy in data["items"]:
-                salary = vacancy["salary"]
-
-                if salary is not None:
-                    if salary["currency"] == "RUR":
-                        if salary["from"] is not None and salary["to"] is not None:
-                            avg_salary = (salary["from"] + salary["to"]) / 2
-                        elif salary["from"] is not None:
-                            avg_salary = salary["from"]
-                        elif salary["to"] is not None:
-                            avg_salary = salary["to"]
-                        else:
-                            continue  # Пропускаем вакансии без указания зарплаты
-                        salaries.append(avg_salary)
-                        vacancies.append(vacancy)
-
-            if len(salaries) > 0:
-                avg_salary = sum(salaries) / len(salaries)
-                high_salary_vacancies = [vacancy for vacancy in vacancies if
-                                         vacancy["salary"] is not None and vacancy["salary"]["currency"] == "RUR" and
-                                         vacancy["salary"]["to"] is not None and vacancy["salary"]["to"] > avg_salary]
-                return high_salary_vacancies
-            else:
-                return None
-        else:
-            print("Ошибка при получении данных. Код ошибки:", response.status_code)
-            return None
+        return high_salary_vacancies
 
     def get_vacancies_with_keyword(self, keyword):
-        url = f"https://api.hh.ru/vacancies?text={keyword}"
-        response = requests.get(url)
+        vacancies = self.get_all_vacancies()
+        keyword_vacancies = []
 
-        if response.status_code == 200:
-            data = response.json()
-            vacancies = data["items"]
+        for vacancy in vacancies:
+            title = vacancy['title']
+            if keyword.lower() in title.lower():
+                keyword_vacancies.append(vacancy)
 
-            if len(vacancies) > 0:
-                return vacancies
-            else:
-                return None
-        else:
-            print("Ошибка при получении данных. Код ошибки:", response.status_code)
-            return None
+        return keyword_vacancies
+
+    def insert_data(self, table_name, data):
+        conn = psycopg2.connect(
+            dbname=self.db_name,
+            user=self.db_user,
+            password=self.db_password,
+            host=self.db_host,
+            port=self.db_port
+        )
+        cursor = conn.cursor()
+
+        for vacancy in data:
+            company = vacancy['company']
+            title = vacancy['title']
+            salary = vacancy['salary']
+            link = vacancy['link']
+            query = f"INSERT INTO {table_name} (company, title, salary, link) VALUES ('{company}', '{title}', '{salary}', '{link}')"
+            cursor.execute(query)
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def select_data(self, table_name):
+        conn = psycopg2.connect(
+            dbname=self.db_name,
+            user=self.db_user,
+            password=self.db_password,
+            host=self.db_host,
+            port=self.db_port
+        )
+        cursor = conn.cursor()
+
+        query = f"SELECT * FROM {table_name}"
+        cursor.execute(query)
+        data = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        return data
--- CW_5/src/main.py
+++ CW_5/src/main.py
@@ -1,26 +1,52 @@
 from dbmanager import DBManager
-import requests
+
+def create_table(db_manager, table_name):
+    conn = db_manager.connect()
+    cursor = conn.cursor()
+
+    query = f"CREATE TABLE IF NOT EXISTS {table_name} (id SERIAL PRIMARY KEY, company VARCHAR(255), title VARCHAR(255), salary VARCHAR(255), link VARCHAR(255))"
+    cursor.execute(query)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
 def main():
-    db_manager = DBManager()
-    db_manager.create_database()
-    db_manager.create_tables()
+    db_manager = DBManager("CW_5", "080475", "5432")
+    table_name = "CW_5"
 
-    companies = db_manager.get_companies_and_vacancies_count()
-    if companies:
-        for company, vacancies in companies:
-            db_manager.insert_employer(company, vacancies)
+    create_table(db_manager, table_name)
 
-        vacancies = db_manager.get_all_vacancies()
-        if vacancies:
-            for vacancy in vacancies:
-                db_manager.insert_vacancy(vacancy)
+    # Получение списка всех компаний и количества вакансий
+    companies_and_vacancies = db_manager.get_companies_and_vacancies_count()
+    db_manager.insert_data(table_name, companies_and_vacancies)
 
-        print("Данные успешно добавлены в базу данных")
-    else:
-        print("Не удалось получить данные о работодателях и вакансиях")
+    # Получение списка всех вакансий
+    all_vacancies = db_manager.get_all_vacancies()
+    db_manager.insert_data(table_name, all_vacancies)
 
+    # Получение средней зарплаты по вакансиям
+    avg_salary = db_manager.get_avg_salary()
+    print("Average Salary:", avg_salary)
+
+    # Получение списка вакансий с зарплатой выше средней
+    high_salary_vacancies = db_manager.get_vacancies_with_higher_salary()
+    print("High Salary Vacancies:")
+    for vacancy in high_salary_vacancies:
+        print(vacancy)
+
+    # Получение списка вакансий с ключевым словом "python"
+    keyword_vacancies = db_manager.get_vacancies_with_keyword("python")
+    print("Keyword Vacancies:")
+    for vacancy in keyword_vacancies:
+        print(vacancy)
+
+    # Извлечение данных из таблицы
+    extracted_data = db_manager.select_data(table_name)
+    print("Extracted Data:")
+    for data in extracted_data:
+        print(data)
 
 if __name__ == "__main__":
     main()
