8d3890f50409dbf691c728afd18e3237b35cc9a7
Klasis <d23kv321@gmail.com>
2023-10-05 17:25:39 +0300

    commit

--- CW_5/src/apihh.py
+++ CW_5/src/apihh.py
@@ -1,30 +1,42 @@
-from urllib import response
 import requests
+import settings
+import sqlite3
 
 
 class ApiHH():
+    def __init__(self):
+        self.db_connection = sqlite3.connect(settings.DB_PATH)
+        self.db_cursor = self.db_connection.cursor()
 
-    def get_companies(self, companies_ids):
-        companies = []
-        for company_id in companies_ids:
-            url = f"https://api.hh.ru/companies/{company_id}"
+    def get_employees(self):
+        employer_ids = [
+            78638,  # Тинькофф
+            1740,  # Яндекс
+            3529,  # Сбербанк
+        ]
+
+        for employer_id in employer_ids:
+            url = f'https://api.hh.ru/vacancies?employer_id={employer_id}&per_page=50'
             response = requests.get(url)
-
-            if response.status_code == 200:
-                company_data = response.get()
-                companies.append(company_data)
-            else:
-                print(f"Ошибка при получении данных для компании с ID {company_id}")
-
-        return companies
+            print(response.json())
 
     def get_vacancies_by_company(self, employer_id):
+        url = f'https://api.hh.ru/vacancies?employer_id={employer_id}&per_page=50'
+        response = requests.get(url)
+
         if response.status_code == 200:
             vacancies_data = response.json()
             vacancies = vacancies_data.get("items", [])
         else:
             print(f"Ошибка при получении данных для компании с ID {employer_id}")
+            vacancies = []
 
         return vacancies
+
+    def save_to_database(self, data):
+        # Здесь выполняется сохранение данных в базу данных
+        # Пример:
+        self.db_cursor.execute("INSERT INTO vacancies (data) VALUES (?)", (data,))
+        self.db_connection.commit()
 
 
